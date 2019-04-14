fc9ebd8530f07688de631467af2a4a87269acf2a
screeeen <miguel@screenimplosion.com>
2019-04-15 00:08:29 +0200

    raw html layout and css, test images

--- src/game.js
+++ src/game.js
@@ -26,6 +26,76 @@ Game.prototype.getHand = function(array) {
     let cardToPass = array.pop();
     hand.push(cardToPass);
   }
-  console.log("game-hand: "+ hand);
+  console.log("game-hand: " + hand);
   return hand;
 };
+
+Game.prototype.pickCard = function(slotNumber) {
+  if (slotNumber >= this.hand.length) {
+    console.log(
+      "this card is not in your hand. choose less than: " + this.hand.length
+    );
+    return;
+  }
+  for (var i = 0; i < this.hand.length; i++) {
+    if (i === slotNumber) {
+      console.log("Your movement is: " + this.hand[slotNumber]);
+      this.checkCard(this.hand[slotNumber]);
+      this.hand.splice(i, 1);
+    }
+  }
+};
+
+Game.prototype.checkCard = function(card) {
+  switch (card) {
+    case "fight":
+      this.fight();
+      break;
+    case "hole":
+      this.hole();
+      break;
+    case "life":
+      this.life();
+      break;
+  }
+  this.checkHandStatus();
+  // displayStatus();
+};
+
+Game.prototype.fight = function() {
+  let damage = parseInt(1 + Math.random() * 4);
+  this.player.hp -= damage;
+
+  console.log(
+    "The fight has broke your jaw in pieces and damage is: " +
+      damage +
+      "\n Now your HP is: " +
+      this.player.hp
+  );
+};
+
+Game.prototype.hole = function() {
+  this.player.hp = 0;
+  console.log("You felt into the hole. You are dead.");
+};
+
+Game.prototype.life = function() {
+  this.player.hp = 10;
+  console.log("You ate chicken. Your HP is: " + this.player.HP);
+};
+
+Game.prototype.flush = function() {
+  this.disposal = this.hand.slice();
+  this.hand = [];
+  console.log(
+    "----------FLUSH------------ \n " + " Disposal Card: " + this.disposal
+  );
+};
+
+Game.prototype.checkHandStatus = function(){
+  if (this.hand.length < 2) {
+    this.flush();
+    console.log("hey, Im in check. this is cardStack: " + this.cardStack);
+    this.hand = this.getHand(this.cardStack);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -11,9 +11,93 @@ function main() {
   console.log(player);
   console.log(game);
 
-  console.log("cardstack: " + game.cardsStack);
+  console.log("cardstack: " + typeof game.cardStack + " " + game.cardsStack);
   console.log("hand: " + game.hand);
   console.log("hp: " + game.player.hp);
+
+  //test
+  // game.pickCard(0);
+
+  //create cards and write them into the stack-view
+  function createCardsIntoStack() {
+    let result = "";
+    game.cardStack.forEach(function(ele) {
+      result += `
+      <li style="list-style-image: url(img/${ele}.png)">${ele}
+      </li>`;
+    });
+    return result;
+  }
+
+  function createRoomsIntoMonitor() {
+    let result = "";
+    game.cardStack.forEach(function(ele) {
+      result += `
+            <div class="room" style="background-image: url(img/room_open.png)"></div>
+`;
+    });
+    return result;
+  }
+
+  function createHandView() {
+    let result = "";
+    game.hand.forEach(function(ele) {
+      result += `
+            <div class="hand-card" style="background: url(img/question.png) no-repeat"></div>`;
+    });
+    return result;
+  }
+
+  function createAvatarView() {
+    let result = `<div class="avatar-pic" style="background: url(img/avatar.png) no-repeat"></div> `;
+    return result;
+  }
+
+  // Generate card divs and append them to the stack view
+  let stackView = document.querySelector(".cards-list");
+  stackView.innerHTML = createCardsIntoStack();
+
+  // Generate rooms and append them to the monitor view
+  let monitorView = document.querySelector("#monitor");
+  monitorView.innerHTML = createRoomsIntoMonitor();
+
+  // Generate hand 
+  let handView = document.querySelector("#hand");
+  handView.innerHTML = createHandView();
+
+  // Generate avatar 
+  let avatarView = document.querySelector("#avatar");
+  avatarView.innerHTML = createAvatarView();
+
+
+
+  function checkGameStatus() {
+    if (game.player.hp < 1) {
+      console.log("GAME OVER");
+      // resetGame();
+      // run();
+    }
+  }
+
+  document.addEventListener("keydown", function(event) {
+    console.log(event.keyCode);
+    if (event.keyCode === 38) {
+      game.pickCard(0);
+    }
+  });
 }
 
 window.addEventListener("load", main);
+
+// garbabe
+/*{ <div id="stack ul" data-card-name="${ele.name}">
+<div class="card-type" name="${ele.img}"></div>
+<div class="icon" style="background: url(img/${ele.img}) no-repeat"> </div>
+</div> }*/
+
+
+//       <div class="room" style=background:url(/../../img/room.png)>
+//       <img src= url(/../../img/room.png)>
+//       </div>
+
+//<div class="card-back" style="background: url(img/question.png) no-repeat"><div>
